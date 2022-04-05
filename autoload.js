2b96fa2e93d64472aafdeb54c9e926418a081233
jizizr <78908842+jizizr@users.noreply.github.com>
2022-04-05 13:26:51 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,5 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://github.com/jizizr/live2d-widget2/raw/main/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/jizizr/live2d-widget2@main/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
